0478c7cc786f17d896c2eae6c848c95f75671a79
walissongpi <walissongpi@gmail.com>
2024-04-23 10:22:24 -0300

    Add files via upload

--- execute.sh
+++ execute.sh
@@ -3,5 +3,4 @@ execute=$1
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.7/lib64
 export PATH=$PATH:/usr/local/cuda-11.7/bin
 export PATH=$PATH:/home/ubuntu/MASA-CUDAlign/masa-cudalign-4.0.2.1028
-export PATH=$PATH:/home/walisson/fickett/MASA-CUDAlign/masa-cudalign-4.0.2.1028
 $execute
